b8bc138b14ac96d9e3a059ba72359b73fa9f43c3
Rob Tallguy <tallguyking@gmail.com>
2020-11-28 14:28:38 -0800

    basic dropping

--- middle.py
+++ middle.py
@@ -1,4 +1,6 @@
+import random
 import socket
+import struct
 import time
 
 ServerIP = "127.0.0.1"
@@ -10,6 +12,8 @@ ClientPort = 20002
 RecevingPort = 20003
 
 bufferSize = 1024
+
+dropChance = 0.1
 
 msgFromServer = "Hello UDP Client"
 bytesToSend = str.encode(msgFromServer)
@@ -26,14 +30,27 @@ while(True):
     print(bytesAddressPair[1][1])    
     #receives from client
     if(bytesAddressPair[1][1] == 20001):
-        print('sent from server')
         message = bytesAddressPair[0]
-        time.sleep(.5)
+        if(round(random.uniform(0, 1.0),2) <= dropChance):
+            msg = struct.unpack('I I 20s I I', message)
+            print('packet ' + str(msg[4]) + ' dropped')
+            #if you're wanting it to go through, its
+            #message = struct.pack('I I 20s I I', msg[0], msg[1], msg[2], msg[3], msg[4])
+            message = struct.pack('I I 20s I I', 0, 0, b'', 0, 0)
+        print('sent from server')
         UDPSocket.sendto(message, (ClientIP, ClientPort))
+        time.sleep(.5)
+
     #receves from server
     elif (bytesAddressPair[1][1] == 20002):
         print('Sent from client')
         message = bytesAddressPair[0]
+        if(round(random.uniform(0, 1.0),2) <= dropChance):
+            msg = struct.unpack('I I 20s I I', message)
+            print('packet ' + str(msg[4]) + ' dropped')
+            #if you're wanting it to go through, its
+            #message = struct.pack('I I 20s I I', msg[0], msg[1], msg[2], msg[3], msg[4])
+            message = struct.pack('I I 20s I I', 0, 0, b'', 0, 0)
         time.sleep(.25)
         UDPSocket.sendto(message, (ServerIP, ServerPort))
     else:
